3c37ad86b24c18359bcb84c80a74bbb246dbb57a
Ant89ne <antoine.bralet@cpe.fr>
2020-12-18 16:53:32 +0100

    Acquisition camera

--- PythonFiles/main.py
+++ PythonFiles/main.py
@@ -3,19 +3,18 @@ import cv2
 import matplotlib.pyplot as plt
 
 def main():
-    cap = cv2.VideoCapture(-1)
+    cap = cv2.VideoCapture(0)
 
     while True :
         ret,frame = cap.read()
 
-        cv2.imshow('Capture Video', frame)
+        key = cv2.waitKey(1)
 
-        #Capture interaction utilisateur
-        key = cv2.waitKey(1) #on évalue la touche pressée
-        plt.pause(0.001)
-        plt.cla()
-        if key & 0xFF == ord('q'): #si appui sur'q'
-            break #sortie de la boucle while
+        if key & 0xFF == ord('q'):
+            break
+
+
+        cv2.imshow('Capture Video', frame)
 
     cap.release()
     cv2.destroyAllWindows()
