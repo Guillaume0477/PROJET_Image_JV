a3da36ea8c700df95f9782d15bde64c4e1196aee
Ant89ne <antoine.bralet@cpe.fr>
2020-12-18 17:49:57 +0100

    Debut du calibrage : creation et deplacement/resize d'un carre

--- PythonFiles/Calibrage.py
+++ PythonFiles/Calibrage.py
@@ -1,8 +1,66 @@
 import cv2
 
-def getWebcamImage():
+def HandCalibrate(cap):
 
-    
+    print("Put your hand inside the square and push 'A'")
+
+    acq = True
+
+    squareSize = [100,100]
+    squareOffset = [200,200]
+
+    while acq :
+        #Get the frame
+        ret,frame = cap.read()
 
 
-    return 0
+        #Detect user quit command
+        key = cv2.waitKey(1)
+
+        if key == ord('g'):
+
+
+
+            #End the calibration
+            acq = False
+
+        #Move the square left
+        elif key == ord('q'):
+            squareOffset[1] -= 10
+        #Move the square right
+        elif key == ord('d'):
+            squareOffset[1] += 10
+        #Move the square up
+        elif key == ord('z'):
+            squareOffset[0] -= 10
+        #Move the square down
+        elif key == ord('s'):
+            squareOffset[0] += 10
+        #Increase square size
+        elif key == ord('a'):
+            squareSize[0] -= 20
+            squareSize[1] -= 20
+            squareOffset[1] += 10
+            squareOffset[0] += 10
+        #Reduce square size
+        elif key == ord('e'):
+            squareSize[0] += 20
+            squareSize[1] += 20
+            squareOffset[1] -= 10
+            squareOffset[0] -= 10            
+
+        #Show the frame
+        frame[squareOffset[0]:squareOffset[0]+squareSize[0],squareOffset[1],:] = 255
+        frame[squareOffset[0]:squareOffset[0]+squareSize[0],squareOffset[1]+squareSize[1],:] = 255
+        frame[squareOffset[0],squareOffset[1]:squareOffset[1] + squareSize[1],:] = 255 
+        frame[squareOffset[0]+squareSize[0],squareOffset[1]:squareOffset[1] + squareSize[1],:] = 255 
+
+
+
+        cv2.imshow('Calibrate your hand', frame)
+
+    #Destroy windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+    return [0,0,0], [[0,0], [1,1]]
--- PythonFiles/main.py
+++ PythonFiles/main.py
@@ -3,19 +3,28 @@ import cv2
 import matplotlib.pyplot as plt
 
 def main():
+    #Config cam
     cap = cv2.VideoCapture(0)
+    play = True
 
-    while True :
+
+    colorHand, BBoxHand = Calibrage.HandCalibrate(cap)
+
+
+    while play :
+        #Get the frame
         ret,frame = cap.read()
 
+
+        #Detect user quit command
         key = cv2.waitKey(1)
-
         if key & 0xFF == ord('q'):
-            break
+            play = False
 
-
+        #Show the frame
         cv2.imshow('Capture Video', frame)
 
+    #Destroy windows
     cap.release()
     cv2.destroyAllWindows()
     return 0
