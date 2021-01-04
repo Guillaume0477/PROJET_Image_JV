18411c3ab39a106ce5f573d958d6e4a3c84a060d
Ant89ne <antoine.bralet@cpe.fr>
2021-01-04 10:29:12 +0100

    Threshold specific color

--- PythonFiles/main.py
+++ PythonFiles/main.py
@@ -1,6 +1,7 @@
 import Calibrage
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     #Config cam
@@ -15,14 +16,19 @@ def main():
         #Get the frame
         ret,frame = cap.read()
 
+        tolerance = 60
 
+        segR = np.array(cv2.inRange(frame[:,:,0], colorHand[0]-tolerance, colorHand[0]+tolerance))
+        segR *= np.array(cv2.inRange(frame[:,:,1], colorHand[1]-tolerance, colorHand[1]+tolerance))
+
+        segR *= np.array(cv2.inRange(frame[:,:,2], colorHand[2]-tolerance, colorHand[2]+tolerance))
         #Detect user quit command
         key = cv2.waitKey(1)
         if key & 0xFF == ord('q'):
             play = False
 
         #Show the frame
-        cv2.imshow('Capture Video', frame)
+        cv2.imshow('Capture Video', segR)
 
     #Destroy windows
     cap.release()
